22ff97004a3488d665040b628ae7bcc27be61864
Mahmoud Farahat <m_farahat@outlook.com>
2022-02-08 15:57:03 +0200

    Add project files.

---
+++ DrawWithSignalR/DrawHub.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+
+namespace DrawWithSignalR
+{
+    public class DrawHub:Hub
+    {
+        public void Draw(int previousX,int previousY ,int currentX,int currentY)
+        {
+            this.Clients.All.DrawOnClient(previousX, previousY, currentX, currentY);
+        }
+        public void DrawDot(int currentX, int currentY)
+        {   
+            this.Clients.All.DrawDotOnClient(currentX, currentY);
+        }
+
+        public void DrawImage(string imagePath)
+        {
+            this.Clients.Others.DrawImageOnClient(imagePath);
+        }
+
+    }
+}
---
+++ DrawWithSignalR/Upload.aspx.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace DrawWithSignalR
+{
+    public partial class Upload : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            HttpPostedFile file = HttpContext.Current.Request.Files[0];
+
+            string path = Server.MapPath("images");
+            file.SaveAs(path+"/"+file.FileName);
+
+            Response.Clear();
+            Response.Write("/images/" + file.FileName);
+            Response.Flush();
+            Response.End();
+        }
+    }
+}
